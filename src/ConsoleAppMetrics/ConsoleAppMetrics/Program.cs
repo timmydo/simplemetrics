81d85b1547fe55de1502bb3e8a2bd1712c64fa71
Timmy Douglas <timmy@timmyd.info>
2019-07-15 23:24:54 -0700

    first

---
+++ src/ConsoleAppMetrics/ConsoleAppMetrics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppMetrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ConsoleAppMetrics/ConsoleAppMetrics/QuantileEpsilonPair.cs
@@ -0,0 +1,14 @@
+﻿namespace MyMetrics
+{
+    public struct QuantileEpsilonPair
+    {
+        public QuantileEpsilonPair(double quantile, double epsilon)
+        {
+            Quantile = quantile;
+            Epsilon = epsilon;
+        }
+
+        public double Quantile { get; }
+        public double Epsilon { get; }
+    }
+}
